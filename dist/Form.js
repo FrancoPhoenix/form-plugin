6a1948b566a7d6d527b6e207ab19adce5ec01660
Franco RD <franco.phoenix@gmail.com>
2017-08-04 11:08:22 -0500

    update files

--- dist/Form.js
+++ dist/Form.js
@@ -7,41 +7,90 @@ class Form {
      * @param {object} data
      */
     constructor(data) {
-        this.originalData = data;
+        this.originalData = data
+        this.inProgress = false
+        this.formData = false
 
         for (let field in data) {
-            this[field] = data[field];
+            this[field] = data[field]
         }
 
-        this.errors = new Errors();
+        this.errors = new Errors()
     }
-
 
     /**
      * Fetch all relevant data for the form.
      */
     data() {
-        let data = {};
+        let data = {}
 
         for (let property in this.originalData) {
-            data[property] = this[property];
+            data[property] = this[property]
         }
 
-        return data;
+        return data
     }
 
+    /**
+     * Fetch all relevant data for the form in FormData.
+     */
+    dataForm () {
+        let data = new FormData()
+
+        for (let property in this.originalData) {
+            switch(true) {
+                case typeof this[property] === 'boolean':
+                    data.append(property, this[property] ? '1' : '0')
+
+                    break
+                case this[property] instanceof FileList:
+
+                    for (let file of this[property]) {
+                        data.append(property + '[]', file)
+                    }
+
+                    break
+                case this[property] === null:
+                    data.append(property, '')
+
+                    break
+                default:
+                    data.append(property, this[property])
+            }
+        }
+
+        return data
+    }
+
+    /**
+     * Load all relevant data for the form.
+     *
+     * @param {object} data
+     */
+    load (data) {
+        for (let property in this.originalData) {
+            if (typeof data[property] !== 'undefined') {
+                this[property] = data[property]
+            }
+        }
+    }
 
     /**
      * Reset the form fields.
      */
     reset() {
-        for (let field in this.originalData) {
-            this[field] = '';
+        if (typeof CKEDITOR != 'undefined') {
+            for (let editor in CKEDITOR.instances) {
+                CKEDITOR.instances[editor].setData( '', function() { this.updateElement() })
+            }
         }
 
-        this.errors.clear();
-    }
+        for (let field in this.originalData) {
+            this[field] = ''
+        }
 
+        this.errors.clear()
+    }
 
     /**
      * Send a POST request to the given URL.
@@ -49,9 +98,8 @@ class Form {
      * @param {string} url
      */
     post(url) {
-        return this.submit('post', url);
+        return this.submit('post', url)
     }
-
 
     /**
      * Send a PUT request to the given URL.
@@ -59,9 +107,8 @@ class Form {
      * @param {string} url
      */
     put(url) {
-        return this.submit('put', url);
+        return this.submit('put', url)
     }
-
 
     /**
      * Send a PATCH request to the given URL.
@@ -69,9 +116,8 @@ class Form {
      * @param {string} url
      */
     patch(url) {
-        return this.submit('patch', url);
+        return this.submit('patch', url)
     }
-
 
     /**
      * Send a DELETE request to the given URL.
@@ -79,9 +125,8 @@ class Form {
      * @param {string} url
      */
     delete(url) {
-        return this.submit('delete', url);
+        return this.submit('delete', url)
     }
-
 
     /**
      * Submit the form.
@@ -90,43 +135,30 @@ class Form {
      * @param {string} url
      */
     submit(requestType, url) {
-        return new Promise((resolve, reject) => {
-            axios[requestType](url, this.data())
-                .then(response => {
-                    this.onSuccess(response.data);
+        this.inProgress = true
+        let data = this.data()
 
-                    resolve(response.data);
+        if (this.formData && (requestType == 'patch' || requestType == 'put')) {
+            data = this.dataForm()
+            data.append('_method', requestType)
+            requestType = 'post'
+        }
+
+        return new Promise((resolve, reject) => {
+            axios[requestType](url, data)
+                .then(response => {
+                    this.inProgress = false
+
+                    resolve(response.data)
                 })
                 .catch(error => {
-                    this.onFail(error.response.data);
+                    this.inProgress = false
+                    this.errors.record(error.response.data)
 
-                    reject(error.response.data);
-                });
-        });
-    }
-
-
-    /**
-     * Handle a successful form submission.
-     *
-     * @param {object} data
-     */
-    onSuccess(data) {
-        alert(data.message); // temporary
-
-        this.reset();
-    }
-
-
-    /**
-     * Handle a failed form submission.
-     *
-     * @param {object} errors
-     */
-    onFail(errors) {
-        this.errors.record(errors);
+                    reject(error.response.data)
+                })
+        })
     }
 }
-
 
 export default Form
